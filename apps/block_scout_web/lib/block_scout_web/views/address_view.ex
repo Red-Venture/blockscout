6556da7b7cb6f10dbe56cc2d2230834c70a562db
nikitosing <nikitosing4@mail.ru>
2021-09-13 22:42:03 +0300

    fix

--- apps/block_scout_web/lib/block_scout_web/views/address_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_view.ex
@@ -232,9 +232,9 @@ defmodule BlockScoutWeb.AddressView do
     Enum.any?(address.smart_contract.abi, &is_read_function?(&1))
   end
 
-  def is_read_function?(function), do: Helper.queriable_method?(function) || Helper.read_with_wallet_method?(function)
-
   def smart_contract_with_read_only_functions?(%Address{smart_contract: nil}), do: false
+
+  def is_read_function?(function), do: Helper.queriable_method?(function) || Helper.read_with_wallet_method?(function)
 
   def smart_contract_is_proxy?(%Address{smart_contract: %SmartContract{}} = address) do
     Chain.proxy_contract?(address.hash, address.smart_contract.abi)
